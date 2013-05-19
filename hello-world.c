666270fe5f920a3c1cd5ffa439c97e2f5602e12b
Horaira <dev.khan@gmail.com>
2013-05-19 15:00:15 +0530

    after creating experiment branch I have modified the master branch

--- hello-world.c
+++ hello-world.c
@@ -4,5 +4,6 @@ int main()
 {
   printf(STRING);
   printf("This line has been added after cloning from repository");
+  printf("Hey in master branch new added fil in experiment branch is not there COOL!!");
   return 0;
   }
